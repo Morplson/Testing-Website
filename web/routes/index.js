4bad37f5a3af14c14d9696ea0ea089214b83ae1b
Morplson <morplson@gmail.com>
2021-03-02 21:25:20 +0100

    ysdf

--- web/routes/index.js
+++ web/routes/index.js
@@ -1,8 +1,16 @@
 exports.index = function(req, res, next) {
-
 	let fingerprint = req.fingerprint;
-
 	
 	res.render('index', { title: 'Hello World!', fingerprint: JSON.stringify(fingerprint) });
 }
 
+exports.bingus = function(req, res, next) {
+	
+	res.render('bingus', { title: 'Bingus'});
+}
+
+
+exports.trolled = function(req, res, next) {
+	
+	res.render('trolled', { title: 'LOL'});
+}
